b448839e8d1a014177b9abb0f03779dfb214cc22
aristotelesbr <aristotelesbr2014@gmail.com>
2017-03-07 16:00:44 -0300

    Studing optional true for associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,20 @@
 class Contact < ApplicationRecord
-	belongs_to :kind
+	belongs_to :kind #, optional: true
 
-	def author
-		"Ari"
-	end
+	# def author
+	# 	"Ari"
+	# end
 
-	def as_json(options={})
-		super(methods: :author, root: true)
-	end
+	# def kind_description
+	# 	self.kind.description
+	# end
+
+	# def as_json(options={})
+	# 	super(
+	# 		methods: [:kind_description, :author],
+	# 	 	root: true,
+	# 	 	# Include "aninha" a informação do modelo relacionado.
+	# 	 	include: { kind: { only: :description} }
+	# 	)
+	# end
 end
