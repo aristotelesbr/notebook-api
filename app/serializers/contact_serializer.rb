a0d089cabaa904c54d47b3f7e0e2c50d374c3679
aristotelesbr <aristotelesbr2014@gmail.com>
2017-04-30 10:20:44 -0300

    AMS and Links(HATEOAS)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,15 +1,29 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate#, :author
 
-	belongs_to :kind
+	belongs_to :kind do
+		link(:self) { contact_url(object.id) }
+	end
 	has_many :phones
 	has_one :address
+
+	link(:self) { contact_url(object.id) }
+	link(:kind) { kind_url(object.kind.id) }
 
 	# def attributes(*args)
 	# 	h = super(*args)
 	# 	h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
 	# 	h
 	# end
+
+	# def author
+	# 	'Aristóteles'
+	# end
+
+	# This is used for all actions, of meta data
+	meta do
+		{author: 'Aristóteles'}
+	end
 
 	# JSON API format
 
